338f3ac5dfcd689939c01db138274db9d519debb
rishithsinghagra <163369562+rishithsinghagra@users.noreply.github.com>
2024-06-07 11:57:39 +0530

    Pseudocode for Google Search
    title

---
+++ testing/main.py
@@ -0,0 +1,21 @@
+# input main_query_nl
+
+# query_breakdown_agent(main_query_nl) -> (subqueries_nl)
+
+# query_cleaner_agent(subqueries_nl) -> (subqueries_cleaned)
+
+# for subquery in subqueries_cleaned:
+
+#     fetch_results_and_clean(subquery) -> (temp_cleaned)
+
+#     subquery_results[subquery] = temp_cleaned
+
+# research_interpreter_agent(main_query_nl, subquery_results)
+
+
+import openai
+
+
+def main():
+    pass
+
